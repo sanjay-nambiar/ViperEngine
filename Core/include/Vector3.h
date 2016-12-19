9b75e2c0a3dc8a21bf3d3632fdd4306343ddfa4e
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2016-12-19 01:26:07 -0500

    Config changes to Core

--- Core/include/Vector3.h
+++ Core/include/Vector3.h
@@ -1,13 +1,19 @@
 #ifndef VECTOR3_HEADER
 #define VECTOR3_HEADER
 
+#include "Types.h"
+
 namespace Viper
 {
 	struct Vector3
 	{
-		float x;
-		float y;
-		float z;
+		float32_t x;
+		float32_t y;
+		float32_t z;
+
+		inline Vector3(float32_t x, float32_t y, float32_t z) : x(x), y(y), z(z)
+		{			
+		}
 	};
 }
 
