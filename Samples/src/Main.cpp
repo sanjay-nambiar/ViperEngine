7a0b829456e65109dda56715c6f64210333d8910
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2017-05-04 01:27:00 -0400

    Fmod test sample

--- Samples/src/Main.cpp
+++ Samples/src/Main.cpp
@@ -1,32 +1,23 @@
 
-#include "FmodAudioManager.h"
 #include <cstdio>
 #include <windows.h>
-//#include <memory>
+#include "FmodAudioManager.h"
 #include "MemoryManager.h"
-//#include "DataArray.h"
-#include "Types.h"
+#include "service/AudioManager.h"
 
-//using namespace Viper::Audio;
 
 using namespace Viper;
 
 
 int main()
 {
-	Audio::FmodAudioManager::CreateInstance();
-	Audio::FmodAudioManager* manager = Audio::FmodAudioManager::GetInstance();
-	manager->Init(10);
+	MemoryManager allocator;
+	AudioManager* manager = new Audio::FmodAudioManager(100, allocator);
 
 	manager->SetListener3dAttributes(Viper::Vector3(0, 0, 0), Viper::Vector3(0, 0, 1), Viper::Vector3(0, 1, 0));
-
-	/*manager->LoadSound("sounds/gattling-gun.wav", true, true, false);
-	manager->LoadSound("sounds/pistol.wav", true, true, false);
-	manager->PlaySound("sounds/gattling-gun.wav");
-	manager->PlaySound("sounds/pistol.wav");*/
-	manager->LoadBank("sounds/ZombieWars.bank");
-	manager->LoadBank("sounds/ZombieWars.strings.bank");
-	manager->LoadEvent("event:/GattlingGun-Fire");
+	manager->LoadSoundBank("sounds/ZombieWars.bank");
+	manager->LoadSoundBank("sounds/ZombieWars.strings.bank");
+	manager->LoadSoundBankEvents("sounds/ZombieWars.bank");
 
 	Viper::Vector3 position(0, 0, 0);
 	manager->SetEvent3dAttributes("event:/GattlingGun-Fire", position, Viper::Vector3(0, 0, 0));
